a0218bd3deb9456137f8f10b9dad4ab4bb1b4c0a
Captured-Heart <knkpozi@gmail.com>
2024-06-16 20:33:59 +0100

    created: AppColors class

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const List<Color> bgGradient = [
+    scaffoldWhite,
+    scaffoldWhite,
+    primaryContainerLight,
+    primaryContainer,
+    primaryContainer,
+  ];
+  static const Color secondary = Color(0xFFA5957E);
+  static const Color secondaryContainer = Color(0xFFD3C4B4);
+
+  static const Color primary = Color(0xFFFC9E12); //
+
+  static const Color primaryContainer = Color(0xFFFAD8B0);
+  static const Color primaryContainerLight = Color(0xFFF6DDC1);
+
+  static const Color scaffoldWhite = Color(0xFFF8F8F7);
+
+  static const Color black = Color(0xFF232220);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color error = Color(0xFFF44336);
+  static const Color successColor = Color(0xFF4CAF50);
+}
