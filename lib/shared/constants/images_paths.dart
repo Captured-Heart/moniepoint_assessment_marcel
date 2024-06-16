61ed4e22df283ed7a4277de09050bce8f691dd12
Captured-Heart <knkpozi@gmail.com>
2024-06-16 20:34:38 +0100

    created: ImagePaths class for images, icons, json in the assets folder

---
+++ lib/shared/constants/images_paths.dart
@@ -0,0 +1,19 @@
+class ImagesPaths {
+  static const String home = 'assets/icons/home.svg';
+  static const String heart = 'assets/icons/heart.svg';
+  static const String profile = 'assets/icons/profile.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String mapPoint = 'assets/icons/mapPoint.svg';
+
+  static const String chat = 'assets/icons/chat.svg';
+  static const String face = 'assets/images/face.png';
+  static const String furniture1 = 'assets/images/furniture1.jpg';
+  static const String furniture2 = 'assets/images/furniture2.jpg';
+  static const String furniture3 = 'assets/images/furniture3.jpg';
+  static const String furniture4 = 'assets/images/furniture4.jpg';
+
+
+  // map dark mode style .json
+  static const String darkModeMap = 'assets/json/map_style.json';
+
+}
