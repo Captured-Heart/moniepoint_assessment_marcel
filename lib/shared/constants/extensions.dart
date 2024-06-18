bd574b170d9078e2af772390daf08ddadff42ade
Captured-Heart <knkpozi@gmail.com>
2024-06-18 12:32:02 +0100

    added: animate to widget extensions

--- lib/shared/constants/extensions.dart
+++ lib/shared/constants/extensions.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 extension PaddingExtension on Widget {
   Widget padAll(double padding) {
@@ -167,4 +168,108 @@ extension MediaQuerySizeExtension on BuildContext {
 
   ColorScheme get colorScheme => theme.colorScheme;
   TextTheme get textTheme => theme.textTheme;
+}
+
+extension WidgetAnimation on Widget {
+  Animate fadeInFromTop({
+    Duration? delay,
+    Duration? animationDuration,
+    Offset? offset,
+  }) =>
+      animate(delay: delay ?? 300.ms)
+          .move(
+            duration: animationDuration ?? 300.ms,
+            begin: offset ?? const Offset(0, -10),
+          )
+          .fade(duration: animationDuration ?? 300.ms);
+
+  Animate fadeInFromBottom({
+    Duration? delay,
+    Duration? animationDuration,
+    Offset? offset,
+  }) =>
+      animate(delay: delay ?? 300.ms)
+          .move(
+            duration: animationDuration ?? 300.ms,
+            begin: offset ?? const Offset(0, 10),
+          )
+          .fade(duration: animationDuration ?? 300.ms);
+
+  // write fadeInFromRight
+
+  Animate fadeInFromLeft({
+    Duration? delay,
+    Duration? animationDuration,
+    Offset? offset,
+  }) =>
+      animate(delay: delay ?? 300.ms)
+          .move(
+            duration: animationDuration ?? 300.ms,
+            begin: offset ?? const Offset(-10, 0),
+          )
+          .fade(duration: animationDuration ?? 300.ms);
+
+  Animate fadeInFromRight({
+    Duration? delay,
+    Duration? animationDuration,
+    Offset? offset,
+  }) =>
+      animate(delay: delay ?? 300.ms)
+          .move(
+            duration: animationDuration ?? 300.ms,
+            begin: offset ?? const Offset(10, 0),
+          )
+          .fade(duration: animationDuration ?? 300.ms);
+
+  Animate fadeIn({
+    Duration? delay,
+    Duration? animationDuration,
+    Curve? curve,
+  }) =>
+      animate(delay: delay ?? 300.ms).fade(
+        duration: animationDuration ?? 300.ms,
+        curve: curve ?? Curves.decelerate,
+      );
+
+  Animate scale({
+    Duration? delay,
+    Duration? animationDuration,
+    Curve? curve,
+  }) =>
+      animate(
+        delay: delay ?? 300.ms,
+      ).scale(
+        duration: animationDuration ?? 300.ms,
+        curve: curve ?? Curves.easeOut,
+      );
+
+  Animate scaleFromLeft({
+    Duration? delay,
+    Duration? animationDuration,
+    Curve? curve,
+  }) =>
+      animate(
+        delay: delay ?? 300.ms,
+      ).scale(
+        duration: animationDuration ?? 3100.ms,
+        curve: curve ?? Curves.easeOut,
+        begin: const Offset(0, -20),
+        // end: const Offset(0, 0),
+        // alignment: Alignment.bottomLeft,
+      );
+
+  Animate slideInFromBottom({
+    Duration? delay,
+    Duration? animationDuration,
+    Curve? curve,
+    double? begin,
+  }) =>
+      animate(
+        delay: delay ?? 300.ms,
+      ).slideY(
+        duration: animationDuration ?? 300.ms,
+        begin: begin ?? 0.2,
+        end: 0,
+        curve: curve ?? Curves.linear,
+      );
 }
