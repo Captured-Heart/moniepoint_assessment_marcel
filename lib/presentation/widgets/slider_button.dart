f138ad4e99ab38da30130ea42ddcdd7fcb9b4670
Captured-Heart <knkpozi@gmail.com>
2024-06-18 12:34:10 +0100

    created: slider_button

--- lib/presentation/widgets/slider_button.dart
+++ lib/presentation/widgets/slider_button.dart
@@ -1,21 +1,31 @@
 import 'dart:async';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:moniepoint_assessment_marcel/shared/constants/extensions.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
-class SliderButton extends StatefulWidget {
-  const SliderButton({
+import '../../app.dart';
+
+class ImgWidget extends StatefulWidget {
+  const ImgWidget({
     super.key,
-    required this.milliseconds,
+    this.milliseconds = 3000,
     required this.text,
+    this.imgHeight,
+    this.imgPath,
+    this.sliderWidth,
   });
 
   final String text;
   final int milliseconds;
+  final String? imgPath;
+  final double? imgHeight;
+  final double? sliderWidth;
+
   @override
-  SliderButtonState createState() => SliderButtonState();
+  ImgWidgetState createState() => ImgWidgetState();
 }
 
-class SliderButtonState extends State<SliderButton> {
+class ImgWidgetState extends State<ImgWidget> {
   bool _isTransformed = false;
   bool _hideText = true;
   int _milliseconds = 200;
@@ -36,56 +46,78 @@ class SliderButtonState extends State<SliderButton> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      width: _isTransformed ? context.sizeWidth(0.8) : 45,
-      height: 42,
-      onEnd: () {
-        setState(() {
-          _hideText = false;
-        });
-      },
-      decoration: BoxDecoration(
-        color: context.colorScheme.primaryContainer.withOpacity(0.8),
-        borderRadius: BorderRadius.circular(_isTransformed ? 100 : 10),
-      ),
-      duration: const Duration(seconds: 2),
-      curve: Curves.easeInOut,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          Align(
-            child: _hideText
-                ? const SizedBox.shrink()
-                : Text(
-                    widget.text,
-                    style: context.textTheme.titleLarge?.copyWith(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Image.asset(
+            widget.imgPath ?? ImagesPaths.furniture2,
+            height: widget.imgHeight ?? context.sizeHeight(0.2),
+            width: context.sizeWidth(1),
+            cacheHeight: context.sizeHeight(0.2).floor(),
+            cacheWidth: context.sizeWidth(1).floor(),
+            fit: BoxFit.cover,
           ),
-          Positioned(
-            right: 0,
+        ),
+        Positioned(
+          bottom: 10,
+          left: context.sizeWidth(0.05),
+          child: AnimatedContainer(
+            width: _isTransformed ? context.sizeWidth(widget.sliderWidth ?? 0.8) : 45,
+            height: 42,
+            onEnd: () {
+              setState(() {
+                _hideText = false;
+              });
+            },
+            decoration: BoxDecoration(
+              color: context.colorScheme.primaryContainer.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(_isTransformed ? 100 : 10),
+            ),
+            duration: const Duration(milliseconds: 1200),
+            curve: Curves.linear,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Align(
+                  child: _hideText
+                      ? const SizedBox.shrink()
+                      : AutoSizeText(
+                          widget.text,
+                          style: context.textTheme.titleLarge?.copyWith(
+                            // fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          maxFontSize: 14,
+                          minFontSize: 10,
+                          maxLines: 1,
+                          textAlign: TextAlign.center,
+                        ).fadeInFromLeft(delay: 100.ms, animationDuration: 100.ms),
+                ),
+                Positioned(
+                  right: 0,
 
-            //there will be
-            child: Container(
-              width: 45,
-              height: 40,
-              margin: _isTransformed ? const EdgeInsets.all(2) : null,
-              decoration: BoxDecoration(
-                color: context.colorScheme.surface,
-                shape: BoxShape.circle,
-              ),
-              child: Icon(
-                Icons.chevron_right,
-                size: 20,
-                color: context.colorScheme.onSurface.withOpacity(0.5),
-              ),
+                  //there will be
+                  child: Container(
+                    width: 40,
+                    height: 38,
+                    margin: _isTransformed ? const EdgeInsets.all(2) : null,
+                    decoration: BoxDecoration(
+                      color: context.colorScheme.surface,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      Icons.chevron_right,
+                      size: 20,
+                      color: context.colorScheme.onSurface.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
