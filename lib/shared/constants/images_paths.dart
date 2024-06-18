a322d6e484fff322657fa349f779b87f01ff43eb
Captured-Heart <knkpozi@gmail.com>
2024-06-18 12:33:52 +0100

    updated: image paths

--- lib/shared/constants/images_paths.dart
+++ lib/shared/constants/images_paths.dart
@@ -1,19 +1,26 @@
 class ImagesPaths {
+  //--------------- ICONS ---------------------------
   static const String home = 'assets/icons/home.svg';
   static const String heart = 'assets/icons/heart.svg';
   static const String profile = 'assets/icons/profile.svg';
   static const String search = 'assets/icons/search.svg';
+  static const String search2 = 'assets/icons/search2.svg';
   static const String mapPoint = 'assets/icons/mapPoint.svg';
-
+  static const String layers = 'assets/icons/layers.svg';
+  static const String mapArrow = 'assets/icons/map-arrow.svg';
   static const String chat = 'assets/icons/chat.svg';
+  static const String filter = 'assets/icons/filter.svg';
+  static const String wallet = 'assets/icons/wallet.svg';
+  static const String trash = 'assets/icons/trash.svg';
+  static const String shield = 'assets/icons/shield.svg';
+
+  // ------------------- IMAGES ---------------------
   static const String face = 'assets/images/face.png';
   static const String furniture1 = 'assets/images/furniture1.jpg';
   static const String furniture2 = 'assets/images/furniture2.jpg';
   static const String furniture3 = 'assets/images/furniture3.jpg';
   static const String furniture4 = 'assets/images/furniture4.jpg';
 
-
-  // map dark mode style .json
+  // --------------------- JSON -----------------------
   static const String darkModeMap = 'assets/json/map_style.json';
-
 }
