e1bb9ea3f5ae80d2a2fc87f41b04d44f2f567ca1
Captured-Heart <knkpozi@gmail.com>
2024-06-18 12:31:28 +0100

    updated Appcolors

--- lib/shared/themes/app_colors.dart
+++ lib/shared/themes/app_colors.dart
@@ -13,10 +13,11 @@ class AppColors {
   static const Color secondary = Color(0xFFA5957E);
   static const Color secondaryContainer = Color(0xFFD3C4B4);
 
-  static const Color primary = Color(0xFFFC9E12); //
-
+  static const Color primary = Color(0xFFFC9E12); //#
   static const Color primaryContainer = Color(0xFFFAD8B0);
   static const Color primaryContainerLight = Color(0xFFF6DDC1);
+
+  static const Color tertiary = Color(0xFF797979);
 
   static const Color scaffoldWhite = Color(0xFFF8F8F7);
 
