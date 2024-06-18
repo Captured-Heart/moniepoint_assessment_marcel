2dffb1c34991e44d8d24baa3271143ae64e0adb9
Captured-Heart <knkpozi@gmail.com>
2024-06-18 18:59:13 +0100

    created: InkResponsse widget

---
+++ lib/presentation/widgets/animations/ink_response.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+import '../../../app.dart';
+
+InkResponse inkResponseWidget(
+  int index,
+  BuildContext context, {
+  double? width,
+  double? height,
+  EdgeInsets? padding,
+  Decoration? decoration,
+  bool showRipple = false,
+  required Widget child,
+  required VoidCallback onTap,
+  bool onHideBorder = false,
+  required Animation<double> rippleAnimation,
+  double? strokeWidth,
+}) {
+  return InkResponse(
+    onTap: onTap,
+    containedInkWell: true,
+    highlightShape: BoxShape.circle,
+    splashColor: Colors.transparent,
+    child: AnimatedContainer(
+      duration: 300.ms,
+      padding: padding ?? const EdgeInsets.all(12),
+      curve: Curves.easeInOut,
+      width: width,
+      height: height,
+      decoration: decoration,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          showRipple
+              ? AnimatedBuilder(
+                  animation: rippleAnimation,
+                  builder: (context, child) {
+                    return !onHideBorder
+                        ? const SizedBox.shrink()
+                        : Center(
+                            child: CustomPaint(
+                              painter:
+                                  RipplePainter(rippleAnimation.value, strokeWidth: strokeWidth),
+                            ),
+                          );
+                  },
+                )
+              : const SizedBox.shrink(),
+          child,
+        ],
+      ),
+    ),
+  );
+}
