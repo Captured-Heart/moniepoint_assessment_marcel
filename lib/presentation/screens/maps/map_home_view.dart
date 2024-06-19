40b38e306e488ba5ba793f1a112d32685f168a33
Captured-Heart <knkpozi@gmail.com>
2024-06-19 12:40:05 +0100

    refactored: MapHomeView, introduced mixins

--- lib/presentation/screens/maps/map_home_view.dart
+++ lib/presentation/screens/maps/map_home_view.dart
@@ -1,9 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter/src/scheduler/ticker.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:moniepoint_assessment_marcel/data/map_home_mixins.dart';
 
 import '../../../app.dart';
 
@@ -15,48 +13,7 @@ class MapHomeView extends StatefulWidget {
   State<MapHomeView> createState() => MapSampleState();
 }
 
-class MapSampleState extends State<MapHomeView>
-    with SingleTickerProviderStateMixin {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-  late AnimationController _animationController;
-  bool isExpanded = true;
-
-  String? _darkMapStyle;
-
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 13,
-  );
-
-  @override
-  void initState() {
-    _animationController = AnimationController(
-        vsync: this, duration: 700.ms, reverseDuration: 500.ms);
-    _animationController.addStatusListener((listener) {
-      if (listener == AnimationStatus.dismissed) {
-        isExpanded = false;
-        setState(() {});
-      } else {
-        isExpanded = true;
-        setState(() {});
-      }
-    });
-    _loadMapStyles();
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  Future _loadMapStyles() async {
-    _darkMapStyle = await rootBundle.loadString(ImagesPaths.darkModeMap);
-  }
-
+class MapSampleState extends State<MapHomeView> with MapHomeMixins {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,14 +23,14 @@ class MapSampleState extends State<MapHomeView>
           // google map
           GoogleMap(
             mapType: MapType.normal,
-            style: widget.darkMapStyle ?? _darkMapStyle,
+            style: widget.darkMapStyle ?? darkMapStyle,
             compassEnabled: false,
             mapToolbarEnabled: false,
             zoomControlsEnabled: false,
             myLocationButtonEnabled: false,
-            initialCameraPosition: _kGooglePlex,
+            initialCameraPosition: kGooglePlex,
             onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
+              googleMapController.complete(controller);
             },
           ),
 
@@ -85,7 +42,7 @@ class MapSampleState extends State<MapHomeView>
             left: 30,
             bottom: context.sizeHeight(0.11),
             child: OverlayDialog(
-              animationController: _animationController,
+              animationController: animationController,
             ),
           ),
 
@@ -101,31 +58,8 @@ class MapSampleState extends State<MapHomeView>
     );
   }
 
-// ---------------- EXTENDED RIGHT FAB BUTTON ---------------------
-  Positioned extendedRightFabBTN(BuildContext context) {
-    return Positioned(
-      bottom: context.sizeHeight(0.11),
-      right: 20,
-      child: Card(
-        elevation: 0,
-        shape: const StadiumBorder(),
-        color: context.colorScheme.tertiary.withOpacity(0.6),
-        child: Row(
-                children: [
-          Icon(
-            Icons.sort_rounded,
-            color: context.colorScheme.surface.withOpacity(0.7),
-            size: 18,
-          ),
-          Text(
-            'List of variants',
-            style: context.textTheme.bodySmall?.copyWith(
-              color: context.colorScheme.surface.withOpacity(0.7),
-            ),
-          ),
-        ].rowInPadding(5))
-            .padAll(15),
-      ).scale(animationDuration: 1400.ms, delay: 200.ms),
-    );
+  @override
+  Ticker createTicker(TickerCallback onTick) {
+    return Ticker(onTick, debugLabel: 'created by $this');
   }
 }
