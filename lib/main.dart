a43c9951dc41295130cac939e0c3c85ca5dd4984
Captured-Heart <knkpozi@gmail.com>
2024-06-19 12:39:05 +0100

    refactored: main.dart, added mixins to the class

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 // ignore_for_file: deprecated_member_use
+// ignore_for_file: implementation_imports
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-// ignore: implementation_imports
 import 'package:flutter/src/scheduler/ticker.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:moniepoint_assessment_marcel/presentation/widgets/animations/ink_response.dart';
-import 'package:svg_flutter/svg.dart';
 
 import 'app.dart';
 
@@ -14,139 +10,46 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> with RippleEffectMixin {
+  @override
+  void initState() {
+    setBegin = 30;
+    setEnd = 20;
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppThemeData.lightTheme,
-      home: const BottomNavBarWrapper(),
-    );
-  }
-}
-
-class BottomNavBarWrapper extends StatefulWidget {
-  const BottomNavBarWrapper({super.key});
-
-  @override
-  State<BottomNavBarWrapper> createState() => _BottomNavBarWrapperState();
-}
-
-class _BottomNavBarWrapperState extends State<BottomNavBarWrapper> with RippleEffectMixin {
-  int pageIndex = 2;
-  int numValue1 = 0;
-  int numValue2 = 0;
-  String? _darkMapStyle;
-
-  @override
-  void initState() {
-    setBegin = 30;
-    setEnd = 20;
-    _loadMapStyles();
-
-    Future.delayed(const Duration(milliseconds: 800), () {
-      setState(() {
-        numValue1 = 1034;
-        numValue2 = 2212;
-      });
-    });
-    super.initState();
-  }
-
-  Future _loadMapStyles() async {
-    _darkMapStyle = await rootBundle.loadString(ImagesPaths.darkModeMap);
-  }
-
-  Map<String, String> navbarIcons = {
-    'Search': ImagesPaths.search,
-    'Chat': ImagesPaths.chat,
-    'Home': ImagesPaths.home,
-    'Heart': ImagesPaths.heart,
-    'Profile': ImagesPaths.profile,
-  };
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          // the background
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: AppColors.bgGradient,
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-          ),
-
-          [
-            //map screen
-            MapHomeView(darkMapStyle: _darkMapStyle),
-            //chat screen
-            PagesPlaceholderWidget(navbarIcons: navbarIcons, pageIndex: pageIndex),
-            // home screen
-            const HomeView(),
-            //heart screen
-            PagesPlaceholderWidget(navbarIcons: navbarIcons, pageIndex: pageIndex),
-            // profile screen
-            PagesPlaceholderWidget(navbarIcons: navbarIcons, pageIndex: pageIndex),
-          ][pageIndex],
-          // MapHomeView(),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: SizedBox(
-              height: kBottomNavigationBarHeight * 1.4,
-              width: context.sizeWidth(0.82),
-              child: Card(
-                color: context.colorScheme.onSurface.withOpacity(0.95),
-                shape: const StadiumBorder(),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ...List.generate(
-                        5,
-                        (index) => inkResponseWidget(
-                              index,
-                              context,
-                              child: SvgPicture.asset(
-                                navbarIcons.values.toList()[index],
-                                color: context.colorScheme.surface,
-                                height: pageIndex == index ? 28 : null,
-                              ),
-                              onTap: () {
-                                onTap();
-                                setState(() {
-                                  pageIndex = index;
-                                });
-                              },
-                              rippleAnimation: rippleAnimation,
-                              width: pageIndex == index ? 55 : 47,
-                              height: pageIndex == index ? 55 : 47,
-                              showRipple: pageIndex == index,
-                              onHideBorder: onHideBorder,
-                              decoration: BoxDecoration(
-                                color: pageIndex == index && !onHideBorder
-                                    ? context.colorScheme.primary
-                                    : context.colorScheme.onSurface,
-                                shape: BoxShape.circle,
-                                border: onHideBorder && pageIndex == index
-                                    ? Border.all(color: context.colorScheme.surface, width: 1)
-                                    : null,
-                              ),
-                            )
-
-                        // navBarIcons(index, context),
-                        ),
-                  ],
+      home: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            // the background
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: AppColors.bgGradient,
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
               ),
-            ).padOnly(bottom: context.sizeHeight(0.015)),
-          ).slideInFromBottom(delay: 3000.ms, animationDuration: 2500.ms, begin: 0.9),
-        ],
+            ),
+
+            // bottom nav bar and screens
+            const BottomNavBarWidget(),
+          ],
+        ),
       ),
     );
   }
