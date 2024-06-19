cadbadf9b809571d39156c630eaeff41ec3541a4
Captured-Heart <knkpozi@gmail.com>
2024-06-19 12:39:39 +0100

    refactored: HomeView

--- lib/presentation/screens/home/home_view.dart
+++ lib/presentation/screens/home/home_view.dart
@@ -61,86 +61,8 @@ class _HomeViewState extends State<HomeView> {
           mainAxisSize: MainAxisSize.min,
           children: [
             // saint petersburg row
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                      child: AnimatedSize(
-                        duration: 800.ms,
-                        curve: Curves.linear,
-                        child: Container(
-                          padding: const EdgeInsets.all(12),
-                          width: !expandText ? 10 : context.sizeWidth(0.48),
-                          decoration: BoxDecoration(
-                              color: context.colorScheme.surface,
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: context.colorScheme.secondary
-                                      .withOpacity(0.1),
-                                  blurRadius: 10,
-                                  offset: const Offset(0, 5),
-                                ),
-                              ]),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              SvgPicture.asset(
-                                ImagesPaths.mapPoint,
-                                color: context.colorScheme.secondary,
-                                height: 20,
-                              ).animate().fadeInFromLeft(
-                                  delay: 450.ms, animationDuration: 500.ms),
-                              Flexible(
-                                child: const AutoSizeText(
-                                  'Saint Petersburg',
-                                  maxLines: 1,
-                                ).animate().fadeInFromLeft(
-                                    delay: 600.ms, animationDuration: 800.ms),
-                              )
-                            ].rowInPadding(5),
-                          ),
-                        ),
-                      ).animate(delay: 2000.ms),
-                    ),
-                    const CircleAvatar(
-                      radius: 22,
-                      backgroundImage: AssetImage(ImagesPaths.face),
-                    ).scale(animationDuration: 900.ms),
-                  ],
-                ),
-
-                // hi marina
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'Hi, Marina',
-                      style: context.textTheme.bodyLarge,
-                    ).fadeInFromLeft(delay: 1500.ms),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'let\'s select your',
-                          style: context.textTheme.titleLarge,
-                          textScaleFactor: 1.2,
-                        ).fadeInFromBottom(
-                            delay: 1800.ms, animationDuration: 450.ms),
-                        Text(
-                          'perfect place',
-                          style: context.textTheme.titleLarge,
-                          textScaleFactor: 1.2,
-                        ).fadeInFromBottom(
-                            delay: 2100.ms, animationDuration: 500.ms),
-                      ],
-                    )
-                  ],
-                ),
-              ].columnInPadding(hideCircleRow ? 10 : 20),
-            ).padSymmetric(horizontal: 15, vertical: 10),
+            IstSectionHomeView(expandText: expandText, hideCircleRow: hideCircleRow)
+                .padSymmetric(horizontal: 15, vertical: 10),
             // BUY AND RENT ROW WIDGETS
             Stack(
               children: [
@@ -239,6 +161,96 @@ class _HomeViewState extends State<HomeView> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class IstSectionHomeView extends StatelessWidget {
+  const IstSectionHomeView({
+    super.key,
+    required this.expandText,
+    required this.hideCircleRow,
+  });
+
+  final bool expandText;
+  final bool hideCircleRow;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              child: AnimatedSize(
+                duration: 800.ms,
+                curve: Curves.linear,
+                child: Container(
+                  padding: const EdgeInsets.all(12),
+                  width: !expandText ? 10 : context.sizeWidth(0.48),
+                  decoration: BoxDecoration(
+                      color: context.colorScheme.surface,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: context.colorScheme.secondary.withOpacity(0.1),
+                          blurRadius: 10,
+                          offset: const Offset(0, 5),
+                        ),
+                      ]),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SvgPicture.asset(
+                        ImagesPaths.mapPoint,
+                        color: context.colorScheme.secondary,
+                        height: 20,
+                      ).animate().fadeInFromLeft(delay: 450.ms, animationDuration: 500.ms),
+                      Flexible(
+                        child: const AutoSizeText(
+                          'Saint Petersburg',
+                          maxLines: 1,
+                        ).animate().fadeInFromLeft(delay: 600.ms, animationDuration: 800.ms),
+                      )
+                    ].rowInPadding(5),
+                  ),
+                ),
+              ).animate(delay: 2000.ms),
+            ),
+            const CircleAvatar(
+              radius: 22,
+              backgroundImage: AssetImage(ImagesPaths.face),
+            ).scale(animationDuration: 900.ms),
+          ],
+        ),
+
+        // hi marina
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Hi, Marina',
+              style: context.textTheme.bodyLarge,
+            ).fadeInFromLeft(delay: 1500.ms),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'let\'s select your',
+                  style: context.textTheme.titleLarge,
+                  textScaleFactor: 1.2,
+                ).fadeInFromBottom(delay: 1800.ms, animationDuration: 450.ms),
+                Text(
+                  'perfect place',
+                  style: context.textTheme.titleLarge,
+                  textScaleFactor: 1.2,
+                ).fadeInFromBottom(delay: 2100.ms, animationDuration: 500.ms),
+              ],
+            )
+          ],
+        ),
+      ].columnInPadding(hideCircleRow ? 10 : 20),
     );
   }
 }
