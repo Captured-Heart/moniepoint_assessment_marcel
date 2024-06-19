482a77d9c46301b65d8dcf6e46a6f2f443c45b7a
Captured-Heart <knkpozi@gmail.com>
2024-06-19 12:41:32 +0100

    added: tooltip widget to bottom_nav_bar [inkResponseWidget]

--- lib/presentation/widgets/animations/ink_response.dart
+++ lib/presentation/widgets/animations/ink_response.dart
@@ -3,7 +3,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 
 import '../../../app.dart';
 
-InkResponse inkResponseWidget(
+Widget inkResponseWidget(
   int index,
   BuildContext context, {
   double? width,
@@ -17,38 +17,41 @@ InkResponse inkResponseWidget(
   required Animation<double> rippleAnimation,
   double? strokeWidth,
 }) {
-  return InkResponse(
-    onTap: onTap,
-    containedInkWell: true,
-    highlightShape: BoxShape.circle,
-    splashColor: Colors.transparent,
-    child: AnimatedContainer(
-      duration: 300.ms,
-      padding: padding ?? const EdgeInsets.all(12),
-      curve: Curves.easeInOut,
-      width: width,
-      height: height,
-      decoration: decoration,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          showRipple
-              ? AnimatedBuilder(
-                  animation: rippleAnimation,
-                  builder: (context, child) {
-                    return !onHideBorder
-                        ? const SizedBox.shrink()
-                        : Center(
-                            child: CustomPaint(
-                              painter: RipplePainter(rippleAnimation.value,
-                                  strokeWidth: strokeWidth),
-                            ),
-                          );
-                  },
-                )
-              : const SizedBox.shrink(),
-          child,
-        ],
+  return Tooltip(
+    message: navbarIcons.entries.toList()[index].key,
+    child: InkResponse(
+      onTap: onTap,
+      containedInkWell: true,
+      highlightShape: BoxShape.circle,
+      splashColor: Colors.transparent,
+      child: AnimatedContainer(
+        duration: 300.ms,
+        padding: padding ?? const EdgeInsets.all(12),
+        curve: Curves.easeInOut,
+        width: width,
+        height: height,
+        decoration: decoration,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            showRipple
+                ? AnimatedBuilder(
+                    animation: rippleAnimation,
+                    builder: (context, child) {
+                      return !onHideBorder
+                          ? const SizedBox.shrink()
+                          : Center(
+                              child: CustomPaint(
+                                painter:
+                                    RipplePainter(rippleAnimation.value, strokeWidth: strokeWidth),
+                              ),
+                            );
+                    },
+                  )
+                : const SizedBox.shrink(),
+            child,
+          ],
+        ),
       ),
     ),
   );
