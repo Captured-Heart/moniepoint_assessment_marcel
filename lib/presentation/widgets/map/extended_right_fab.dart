04d3e4a4ffcb36873074266c4ccaf3ce712ef162
Captured-Heart <knkpozi@gmail.com>
2024-06-19 12:42:56 +0100

    created: extended_right_fab widget

---
+++ lib/presentation/widgets/map/extended_right_fab.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:moniepoint_assessment_marcel/app.dart';
+
+Positioned extendedRightFabBTN(BuildContext context) {
+  return Positioned(
+    bottom: context.sizeHeight(0.11),
+    right: 20,
+    child: Card(
+      elevation: 0,
+      shape: const StadiumBorder(),
+      color: context.colorScheme.tertiary.withOpacity(0.6),
+      child: Row(
+              children: [
+        Icon(
+          Icons.sort_rounded,
+          color: context.colorScheme.surface.withOpacity(0.7),
+          size: 18,
+        ),
+        Text(
+          'List of variants',
+          style: context.textTheme.bodySmall?.copyWith(
+            color: context.colorScheme.surface.withOpacity(0.7),
+          ),
+        ),
+      ].rowInPadding(5))
+          .padAll(15),
+    ).scale(animationDuration: 1400.ms, delay: 200.ms),
+  );
+}
