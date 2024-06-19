fb1bc586108bfe6bf34bd7c0922b53e0ea56034d
Captured-Heart <knkpozi@gmail.com>
2024-06-19 12:40:57 +0100

    sorted exports alphabetically

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,8 +1,11 @@
-export 'home/bottom_sheet.dart';
-export 'home/slider_button.dart';
-export 'map/list_of_markers.dart';
-export 'map/search_bar.dart';
-export 'home/pages_placeholder.dart';
+export 'animations/ink_response.dart';
 export 'animations/ripple_effect_mixins.dart';
-export 'map/overlay_dialog.dart';
 export 'animations/ripple_painter.dart';
+export 'bottom_nav_bar.dart';
+export 'home/bottom_sheet.dart';
+export 'home/pages_placeholder.dart';
+export 'home/slider_button.dart';
+export 'map/extended_right_fab.dart';
+export 'map/list_of_markers.dart';
+export 'map/overlay_dialog.dart';
+export 'map/search_bar.dart';
